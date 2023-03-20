0484c59092ed369926989baa32c8dcfe1547df61
James Robertson <digital.robertson@gmail.com>
2023-03-20 09:06:16 +0000

    feature: Added the public method *replace_records* to allow updating DNS ip address for domain name.

--- godaddy23.gemspec
+++ godaddy23.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'godaddy23'
-  s.version = '0.1.1'
+  s.version = '0.2.0'
   s.summary = 'My first exploration into the GoDaddy API to query domains I own etc.'
   s.authors = ['James Robertson']
   s.files = Dir["lib/godaddy23.rb"]
--- lib/godaddy23.rb
+++ lib/godaddy23.rb
@@ -10,6 +10,32 @@
 
 # By default this gem is set for the production environment
 
+# API reference for domains
+
+# available # GET # /v1/domains/available # Determine whether or not the specified domain is available for purchase
+# availables # POST # /v1/domains/available # Determine whether or not the specified domains are available for purchase
+# n/a # POST # /v1/domains/contacts/validate # Validate the request body using the Domain Contact Validation Schema for  specified domains.
+# n/a # POST # /v1/domains/purchase # Purchase and register the specified Domain
+# n/a # GET # /v1/domains/purchase/schema/{tld} # Retrieve the schema to be submitted when registering a Domain for the  specified TLD
+# n/a # POST # /v1/domains/purchase/validate # Validate the request body using the Domain Purchase Schema for the # specified TLD
+# n/a # GET # /v1/domains/suggest # Suggest alternate Domain names based on a seed Domain, a set of keywords, or the # shopper's purchase history
+# n/a # GET # /v1/domains/tlds # Retrieves a list of TLDs supported and enabled for sale
+# n/a # DELETE # /v1/domains/{domain} # Cancel a purchased domain
+# details # GET # /v1/domains/{domain} # Retrieve details for the specified Domain
+# n/a # PATCH # /v1/domains/{domain} # Update details for the specified Domain
+# n/a # PATCH # /v1/domains/{domain}/contacts # Update domain
+# n/a # DELETE # /v1/domains/{domain}/privacy # Submit a privacy cancellation request for the given domain
+# n/a # POST # /v1/domains/{domain}/privacy/purchase # Purchase privacy for a specified domain
+# n/a # PATCH # /v1/domains/{domain}/records # Add the specified DNS Records to the specified Domain
+# n/a # PUT # /v1/domains/{domain}/records # Replace all DNS Records for the specified Domain
+# list_records # GET # /v1/domains/{domain}/records/{type}/{name} # Retrieve DNS Records for the specified Domain, optionally with the specified Type and/or Name
+# replace_records # PUT # /v1/domains/{domain}/records/{type}/{name} # Replace all DNS Records for the specified Domain with the specified Type and Name
+# n/a # DELETE # /v1/domains/{domain}/records/{type}/{name} # Delete all DNS Records for the specified Domain with the specified Type and Name
+# n/a # PUT # /v1/domains/{domain}/records/{type} # Replace all DNS Records for the specified Domain with the specified Type
+# n/a # POST # /v1/domains/{domain}/renew # Renew the specified Domain
+# n/a # POST # /v1/domains/{domain}/transfer # Purchase and start or restart transfer process
+# n/a # POST # /v1/domains/{domain}/verifyRegistrantEmail # Re-send Contact E-mail Verification for specified Domain
+
 
 require 'net/http'
 require 'uri'
@@ -35,7 +61,7 @@ module GoDaddy23
     #
     #
     def details(domain)
-      submit(domain)
+      get(domain)
     end
     
     # Retrieve DNS Records for the specified Domain, optionally with the
@@ -45,7 +71,7 @@ module GoDaddy23
     #   name: e.g. www
     #
     # curl -X 'GET' \
-    #   'https://api.godaddy.com/v1/domains/[MYDOMAIN]' \
+    #   'https://api.godaddy.com/v1/domains/[MYDOMAIN]/records/[TYPE]/[NAME]' \
     #   -H 'accept: application/json' \
     #   -H 'Authorization: sso-key [APIKEY]:[SECRET]
     #
@@ -60,16 +86,49 @@ module GoDaddy23
         uri += '/' + name
       end
       
-      submit(uri)
+      get(uri)
       
     end
     
+    def replace_records(domain, type: nil, name: nil, records: [])
+      
+      uri = domain
+      uri += '/records'
+      uri += '/' + type if type
+      
+      if name then
+        uri += '/A' if type.nil?
+        uri += '/' + name
+      end
+      
+      put(uri, records)
+    end
+    
     private
+    
+    def get(uri_part)
+      r = submit(uri_part) do |uri|
+        Net::HTTP::Get.new(uri)        
+      end
+      JSON.parse(r.body, symbolize_names: true)      
+    end
+    
+    def put(uri_part, payload)
+      
+      r = submit(uri_part) do |uri|
+        request = Net::HTTP::Put.new(uri)        
+        request.content_type = "application/json"
+        request.body = JSON.dump(payload)
+        request
+      end
+      
+    end    
 
     def submit(s)
 
       uri = URI.parse(@url + "/v1/domains/" + s)
-      request = Net::HTTP::Get.new(uri)
+
+      request = yield(uri) if block_given?
       request["Accept"] = "application/json"
       request["Authorization"] = "sso-key %s:%s" % [@apikey, @secret]
 
@@ -81,7 +140,6 @@ module GoDaddy23
         http.request(request)
       end
 
-      JSON.parse(response.body, symbolize_names: true)
 
     end
   end
